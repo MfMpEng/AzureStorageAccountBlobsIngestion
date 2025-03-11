e4a5401084ae004319412becc3c2fbc644f5c221
mfsen10 <30324435+mfsen10@users.noreply.github.com>
2025-03-11 08:21:33 -0500

    runtime code organization, log msg optimization, json prop rename update

--- StorageAccountBlobsIngestion/AzureFunctionBlobsIngestion/run.ps1
+++ StorageAccountBlobsIngestion/AzureFunctionBlobsIngestion/run.ps1
@@ -22,6 +22,22 @@ $WorkspaceId = $env:WorkspaceId
 $Workspacekey = $env:LogAnalyticsWorkspaceKey
 $LATableName = $env:LATableName
 $LAURI = $env:LAURI
+$QueueID = $TriggerMetadata.Id
+$QueuePOP = $TriggerMetadata.PopReceipt
+$AzureStorage = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
+#####Build the JSON from queue and grab blob path vars
+$QueueMsg = ConvertTo-Json $QueueItem -Depth 20
+$QueueArr = @(ConvertFrom-Json $QueueMsg);
+$StorageAccountName = $QueueArr.topic.split('/')[-1]
+$ContainerName = $QueueArr.subject.split('/')[4]
+$BlobName = $QueueArr.subject.split('/')[-1]
+$BlobPath = $QueueArr.subject.split('/')[6..($QueueArr.subject.split('/').Length - 1)] -join '/'
+$evtTime = $QueueArr.eventTime
+$logPath = [System.IO.Path]::Combine($env:TEMP, $BlobName)
+
+# $BlobURL            = $QueueArr.data.url.tostring()
+# $ResourceGroup      = $QueueArr.topic.split('/')[4]
+#check that fn host env vars' workspace ID is valid and that we're sending to LA HTTP REST
 if($LAURI.Trim() -notmatch 'https:\/\/([\w\-]+)\.ods\.opinsights\.azure.([a-zA-Z\.]+)$')
 {
     Write-Error -Message "Storage Account Blobs Ingestion: Invalid Log Analytics Uri." -ErrorAction Stop
@@ -161,8 +177,6 @@ Function Write-OMSLogfile {
         Write-Verbose -Message ('Post Function Return Code ' + $response.statuscode)
         return $response.statuscode
     }
-    # Add DateTime to hashtable
-    #$logdata | Add-Member -MemberType NoteProperty -Name "TimeGenerated" -Value $evtTime
     #Build the JSON file
     $logMessage = ($logdata | ConvertTo-Json -Depth 20)
     Write-Verbose -Message ("Log Message POST Body:`n" + $logMessage)
@@ -197,91 +211,19 @@ Function Submit-ChunkLAdata ($corejson, $customLogName) {
     }
 }
 function Rename-JsonProperties {
-    param (
-        [string]$rawJson,
-        [hashtable]$newNames
-    )
+    param ([string]$rawJson )
     # Convert the raw JSON primitive to a PowerShell object
-    $data = $rawJson | ConvertFrom-Json
-    $data | Add-Member -MemberType NoteProperty -Name "TimeGenerated" -Value $data."@timestamp" -Force
-    # Rename the properties
-    foreach ($obj in $data) {
-        foreach ($oldName in $newNames.Keys) {
-            if ($obj | Select-Object -ExpandProperty $oldName) {
-                $obj | Add-Member -MemberType NoteProperty -Name $newNames[$oldName] -Value ($obj | Select-Object -ExpandProperty $oldName)
-                $obj.PSObject.Properties.Remove($oldName)
-            }
-        }
-    }
-    # Convert the updated data back to JSON without compression
-    $updatedJson = $data | ConvertTo-Json
-    return $updatedJson
-}
-
-# # Execution
-#Build the JSON from queue and grab blob path vars
-$QueueMsg           = ConvertTo-Json $QueueItem -Depth 20 #-Compress -Verbose
-$QueueArr           = @(ConvertFrom-Json $QueueMsg);
-$StorageAccountName = $QueueArr.topic.split('/')[-1]
-$ContainerName      = $QueueArr.subject.split('/')[4]
-$BlobName           = $QueueArr.subject.split('/')[-1]
-$BlobPath           = $QueueArr.subject.split('/')[6..($QueueArr.subject.split('/').Length - 1)] -join '/'
-$evtTime            = $QueueArr.eventTime
-$QueueID            = $TriggerMetadata.Id
-$QueuePOP           = $TriggerMetadata.PopReceipt
-# $BlobURL            = $QueueArr.data.url.tostring()
-# $ResourceGroup      = $QueueArr.topic.split('/')[4]
-Write-Host ("######################################################################################")
-Write-Host ("######################### BEGIN NEW TRANSACTION ######################################")
-Write-Host ("################# $BlobName ################")
-Write-Host ("######################################################################################")
-Write-Host ("PowerShell queue trigger function processed work item:" + $QueueItem)
-Write-Host ("Current Directory           :" + $(Get-Location))
-Write-Host ("Queue item expiration time  :" + $TriggerMetadata.ExpirationTime)
-Write-Host ("Queue item insertion time   :" + $TriggerMetadata.InsertionTime)
-Write-Host ("Queue item next visible time:" + $TriggerMetadata.NextVisibleTime)
-Write-Host ("Queue Message ID            :" + $QueueId)
-Write-Host ("Queue Message Pop receipt   :" + $QueuePOP)
-Write-Host ("Dequeue count               :" + $TriggerMetadata.DequeueCount)
-Write-Host ("Log Analytics URI           :" + $LAURI)
-Write-Host ("$evtTime Queue Reported new item`nStorage Account Name     Container Name     BlobName`n$StorageAccountName  \  $ContainerName  \  $BlobName")
-$AzureStorage = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
-$logPath = [System.IO.Path]::Combine($env:TEMP, $BlobName)
-# Get-AzStorageBlobContent -Context $AzureStorage -Container $ContainerName -Blob $BlobPath -Destination $logPath -force > $null
-if ($BlobPath -like "concurrencyStatus.json") {
-    Write-Verbose "Ignoring Concurrency Status file"
-    $skipfile = 1;
-    $skipNonLog = 1;
-}
-if (!$skipNonLog){
-    try {
-        Write-Output "Attempting to download blob content..."
-        Get-AzStorageBlobContent -Context $AzureStorage -Container $ContainerName -Blob $BlobPath -Destination $logPath -Force |out-null
-        Write-Output "Blob content downloaded to $logPath"
-    }
-    catch {
-        Write-Output "Error downloading blob content: $_"
-    }
-    $logsFromFile = Get-Content -Path $logPath -Raw
-    if ($logsFromFile.length -eq 0 -or $null -eq $logsFromFile) {
-        Write-Verbose "Ignoring empty logfile";
-        $skipfile = 1;
-    }
-}
-if (!$skipfile -and !$skipNonLog) {
+    $modJson = $rawJson | ConvertFrom-Json
+    $modJson | Add-Member -MemberType NoteProperty -Name "TimeGenerated" -Value $modJson."@timestamp" -Force
     # TODO: param-ize custom prop name list
-    [hashtable]$PropNameReplDict = [ordered]@{
-        "app_type"                  = "app_type_CF";
-        "dst"                       = "dst_CF";
-        "dst_instance"              = "dst_instance_CF";
-        "dst_site"                  = "dst_site_CF";
-        "no_active_detections"      = "no_active_detections_CF";
-        "req_params"                = "req_params_CF";
-        "@timestamp"                = "F5_timestamp_CF";
+    [hashtable]$logToTablePropNames = [ordered]@{
         "_id"                       = "F5_id_CF";
         "_visitor_id"               = "F5_visitor_id_CF";
+        "@timestamp"                = "F5_timestamp_CF";
         "action"                    = "action_CF";
         "api_endpoint"              = "api_endpoint_CF";
+        "app_firewall_name"         = "app_firewall_name_CF"; # Added property
+        "app_type"                  = "app_type_CF";
         "app"                       = "app_CF";
         "as_number"                 = "as_number_CF";
         "as_org"                    = "as_org_CF";
@@ -298,8 +240,12 @@ if (!$skipfile -and !$skipNonLog) {
         "detections"                = "detections_CF";
         "device_type"               = "device_type_CF";
         "domain"                    = "domain_CF";
+        "dst_instance"              = "dst_instance_CF";
         "dst_ip"                    = "dst_ip_CF";
         "dst_port"                  = "dst_port_CF";
+        "dst_site"                  = "dst_site_CF";
+        "dst"                       = "dst_CF";
+        "enforcement_mode"          = "enforcement_mode_CF"; # Added property
         "excluded_threat_campaigns" = "excluded_threat_campaigns_CF";
         "hostname"                  = "hostname_CF";
         "http_version"              = "http_version_CF";
@@ -312,17 +258,19 @@ if (!$skipfile -and !$skipNonLog) {
         "method"                    = "method_CF";
         "namespace"                 = "namespace_CF";
         "network"                   = "network_CF";
+        "no_active_detections"      = "no_active_detections_CF";
         "original_headers"          = "original_headers_CF";
         "original_path"             = "original_path_CF";
         "path"                      = "path_CF";
         "region"                    = "region_CF";
-        "req_headers"               = "req_headers_CF";
         "req_headers_size"          = "req_headers_size_CF";
+        "req_headers"               = "req_headers_CF";
         "req_id"                    = "req_id_CF";
+        "req_params"                = "req_params_CF";
         "req_path"                  = "req_path_CF";
         "req_size"                  = "req_size_CF";
-        "rsp_code"                  = "rsp_code_CF";
         "rsp_code_class"            = "rsp_code_class_CF";
+        "rsp_code"                  = "rsp_code_CF";
         "rsp_size"                  = "rsp_size_CF";
         "sec_event_name"            = "sec_event_name_CF";
         "sec_event_type"            = "sec_event_type_CF";
@@ -330,19 +278,19 @@ if (!$skipfile -and !$skipNonLog) {
         "signatures"                = "signatures_CF";
         "site"                      = "site_CF";
         "sni"                       = "sni_CF";
-        "src"                       = "src_CF";
         "src_instance"              = "src_instance_CF";
         "src_ip"                    = "src_ip_CF";
         "src_port"                  = "src_port_CF";
         "src_site"                  = "src_site_CF";
+        "src"                       = "src_CF";
         "stream"                    = "stream_CF";
         "tag"                       = "tag_CF";
         "tenant"                    = "tenant_CF";
         "threat_campaigns"          = "threat_campaigns_CF";
         "time"                      = "time_CF";
         "tls_fingerprint"           = "tls_fingerprint_CF";
-        "user"                      = "user_CF";
         "user_agent"                = "user_agent_CF";
+        "user"                      = "user_CF";
         "vh_name"                   = "vh_name_CF";
         "vhost_id"                  = "vhost_id_CF";
         "violation_details"         = "violation_details_CF";
@@ -350,12 +298,71 @@ if (!$skipfile -and !$skipNonLog) {
         "violations"                = "violations_CF";
         "waf_mode"                  = "waf_mode_CF";
         "x_forwarded_for"           = "x_forwarded_for_CF";
+       #"TimeGenerated"             = "TimeGenerated";
     }
-    $encodedJson = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::UTF8.GetBytes($logsFromFile))
-    $newJsonString = Rename-JsonProperties -rawJson $encodedJson -newNames $PropNameReplDict -verbose
-    # $json = Convert-LogLineToJson($log)
-    $LAPostResult = Submit-ChunkLAdata -Verbose -Corejson $newJsonString -CustomLogName $LATableName -verbose
+    # Rename the properties
+    foreach ($jsonProp in $modJson) {
+        foreach ($oldName in $logToTablePropNames.Keys) {
+            if ($jsonProp | Select-Object -ExpandProperty $oldName.Trim() -ErrorAction SilentlyContinue) {
+                $jsonProp | Add-Member -MemberType NoteProperty `
+                    -Name $logToTablePropNames[$oldName] -Value ($jsonProp | Select-Object -ExpandProperty $oldName)
+                $jsonProp.PSObject.Properties.Remove($oldName)
+            }
+        }
+    }
+    # Convert the updated data back to JSON
+    $updatedJson = $modJson | ConvertTo-Json
+    return $updatedJson
 }
+
+##### Execution
+Write-Host ("######################################################################################")
+Write-Host ("######################### BEGIN NEW TRANSACTION ######################################")
+Write-Host ("################# $BlobName ################")
+Write-Host ("######################################################################################")
+Write-Host ("PowerShell queue trigger function processed work item:" + $QueueItem)
+Write-Host ("Current Directory           :" + $(Get-Location))
+Write-Host ("Queue item expiration time  :" + $TriggerMetadata.ExpirationTime)
+Write-Host ("Queue item insertion time   :" + $TriggerMetadata.InsertionTime)
+Write-Host ("Queue item next visible time:" + $TriggerMetadata.NextVisibleTime)
+Write-Host ("Queue Message ID            :" + $QueueId)
+Write-Host ("Queue Message Pop receipt   :" + $QueuePOP)
+Write-Host ("Dequeue count               :" + $TriggerMetadata.DequeueCount)
+Write-Host ("Log Analytics URI           :" + $LAURI)
+Write-Host ("$evtTime Queue Reported new item`nStorage Account Name     Container Name     BlobName`n$StorageAccountName  \  $ContainerName  \  $BlobName")
+
+# Skip processing of non-relevant files that got written to the storage container
+if ($BlobPath -notlike ".log") {
+    Write-Verbose "Ignoring ConcurrencyStatus.json/non-Log file"
+    $skipfile = 1;
+    $skipNonLog = 1;
+}
+# LogFile Collection and check/skip empty
+if (!$skipNonLog){
+    try {
+        Write-Output "Attempting to download blob content..."
+        Get-AzStorageBlobContent -Context $AzureStorage -Container $ContainerName -Blob $BlobPath -Destination $logPath -Force |out-null
+        Write-Output "Blob content downloaded to $logPath"
+    }
+    catch {
+        Write-Output "Error downloading blob content: $_"
+    }
+    $logsFromFile = Get-Content -Path $logPath -Raw
+    if ($logsFromFile.length -eq 0 -or $null -eq $logsFromFile) {
+        Write-Verbose "Ignoring empty logfile";
+        $skipfile = 1;
+    }
+}
+
+if (!$skipfile -and !$skipNonLog) {
+    $encodedJson = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::UTF8.GetBytes($logsFromFile))
+    $renamedJsonPrimative = Rename-JsonProperties -rawJson $encodedJson -verbose
+    # If log source does not contain table headers
+    # $json = Convert-LogLineToJson($log)
+    # Wrapper for write-omslogfile to chunk based on REST API limit spec. Max 30Mb, this cuts at 25.
+    $LAPostResult = Submit-ChunkLAdata -Verbose -Corejson $renamedJsonPrimative -CustomLogName $LATableName -verbose
+}
+
 if($LAPostResult -eq 200 -or $skipfile -eq 1) {
     Remove-Item $logPath
     Write-Output ("Storage Account Blobs ingested into Azure Log Analytics Workspace Table $LATableName")
@@ -364,19 +371,20 @@ if($LAPostResult -eq 200 -or $skipfile -eq 1) {
     Write-Output ("Dequeuing Trigger ID/popReceipt: '$QueueId :: $QueuePop' From CloudQueue '$AzureQueue'")
     $CloudQueue = $AzureQueue.CloudQueue
     Write-Verbose -Message ("Cloud Queue Reference '$CloudQueue' CloudQueue URI '$'${CloudQueue.uri}")
-    if ($null -ne $CloudQueue -and $null -ne $QueueId -and $null -ne $QueuePOP) {
-        try {
+    # if ($null -ne $CloudQueue -and $null -ne $QueueId -and $null -ne $QueuePOP) {
+        # try {
             $CloudQueue.DeleteMessage($QueueID, $QueuePOP)
-        }
-        catch {
-            Write-Warning -Message "Unable to DeQueue Item from: Queue='$CloudQueue' TriggerMetadata ID : '$queueid' QPOP: '$queuepop'"
-        }
-    } else {
-        Write-Warning -Message "Unable to DeQueue Item from: Queue='$CloudQueue' TriggerMetadata ID : '$queueid' QPOP: '$queuepop'"
-    }
+        # }
+        # catch {
+            # Write-Warning -Message "Unable to DeQueue Item from: Queue='$CloudQueue' TriggerMetadata ID : '$queueid' QPOP: '$queuepop'"
+        # }
+    # } else {
+        # Write-Warning -Message "Unable to DeQueue Item from: Queue='$CloudQueue' TriggerMetadata ID : '$queueid' QPOP: '$queuepop'"
+    # }
     Remove-AzStorageBlob -Context $AzureStorage -Container $ContainerName -Blob $BlobPath -verbose
     [System.GC]::collect() #cleanup memory
 }
+
 Write-Host ("######################################################################################")
 Write-Host ("############################ END TRANSACTION #########################################")
 Write-Host ("################# $BlobName ################")
